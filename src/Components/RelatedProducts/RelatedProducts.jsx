550253ff698ab3b77e5ba9c3d539343f8d14e6e1
gn213245 <gn213245@gmail.com>
2024-06-04 00:50:20 +1000

    Description Box

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './RelatedProducts.css'
+
+const RelatedProducts = () => {
+  return (
+    <div className='related-products'>
+      <h1>RELATED PRODUCTS</h1>
+      
+    </div>
+  )
+}
+
+export default RelatedProducts
